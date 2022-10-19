a51313f86d742630c05a1aedc797be6fc045031e
kasabovadiana1 <kasabova.diana@gmail.com>
2022-10-19 18:59:31 +0000

    update outputs 02.02

--- 02-data-types-and-basic-operators/02.02-lab-print-function/deploy/outputs.tf
+++ 02-data-types-and-basic-operators/02.02-lab-print-function/deploy/outputs.tf
@@ -1,5 +1,5 @@
 output "Workspace_for_lab" {
-  value       =  "${format("%s%s%s","https://",azurerm_container_group.containergroup.fqdn,":8443")}"
+  value       =  "${format("%s%s%s","http://",azurerm_container_group.containergroup.fqdn,":8443")}"
   description = "Workspace for lab"
   depends_on  = []
 }
