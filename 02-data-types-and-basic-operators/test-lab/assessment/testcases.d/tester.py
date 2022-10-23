3da40963c3fa29049859b0682e458b293b3438f0
kasabovadiana1 <kasabova.diana@gmail.com>
2022-10-23 16:05:22 +0000

    add test lab

---
+++ 02-data-types-and-basic-operators/test-lab/assessment/testcases.d/tester.py
@@ -0,0 +1,4 @@
+from grader import *
+from grader.extensions import ast
+
+io_test("Program should have correct output", "", "Programming...Essentials...in***Python")
---
+++ 02-data-types-and-basic-operators/test-lab/deploy/solution/example.py
@@ -0,0 +1,8 @@
+import os
+
+def main():
+    print("Programming","Essentials","in")
+    print("Python")
+
+if __name__ == "__main__":
+    main()
