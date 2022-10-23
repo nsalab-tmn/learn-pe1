d02d3cad9de0bc89fbb49ae4d544b110ccef7fea
kasabovadiana1 <kasabova.diana@gmail.com>
2022-10-23 19:18:21 +0000

    update deploy labs

---
+++ 02-data-types-and-basic-operators/02.09-lab-variables/deploy/outputs.tf
@@ -0,0 +1,23 @@
+output "Workspace_for_lab" {
+  value       =  "${format("%s%s","https://",azurerm_dns_a_record.a_record.fqdn)}"
+  description = "Workspace for lab"
+  depends_on  = []
+}
+
+output "learn_rg" {
+  value       =  azurerm_resource_group.learn.name
+  description = "Resource group"
+  depends_on  = []
+}
+
+output "container_group" {
+  value       =  azurerm_container_group.containergroup.name
+  description = "azurerm_container_group"
+  depends_on  = []
+}
+
+output "instance_id" {
+  value       =  "${var.instance_id}"
+  description = "instance_id"
+  depends_on  = []
+}
