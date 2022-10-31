51721ad65e9205944f7823f3f9f2cf6c9f07c186
kasabovadiana1 <kasabova.diana@gmail.com>
2022-10-31 08:17:54 +0000

    update assessment 02.04

--- 02-data-types-and-basic-operators/02.04-lab-print-arguments/deploy-vkcs/outputs.tf
+++ 02-data-types-and-basic-operators/02.04-lab-print-arguments/deploy-vkcs/outputs.tf
@@ -3,3 +3,9 @@ output "URL" {
   description = "Main portal address"
   depends_on  = []
 }
+
+output "ipv4" {
+  value       = ${vkcs_compute_instance.compute[0].access_ip_v4}
+  description = "Ip of vm"
+  depends_on  = []
+}
