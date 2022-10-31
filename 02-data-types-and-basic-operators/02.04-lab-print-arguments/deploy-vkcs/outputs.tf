837354f5b1f87ec44a2dace32ba186dd77ee5547
kasabovadiana1 <kasabova.diana@gmail.com>
2022-10-31 09:20:19 +0000

    update deploy vkcs

--- 02-data-types-and-basic-operators/02.04-lab-print-arguments/deploy-vkcs/outputs.tf
+++ 02-data-types-and-basic-operators/02.04-lab-print-arguments/deploy-vkcs/outputs.tf
@@ -5,7 +5,7 @@ output "URL" {
 }
 
 output "ipv4" {
-  value       = ${vkcs_compute_instance.compute[0].access_ip_v4}
+  value       = "${vkcs_compute_instance.compute[0].access_ip_v4}"
   description = "Ip of vm"
   depends_on  = []
 }
